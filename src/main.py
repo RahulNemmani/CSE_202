f1e0c7131c2d3ef5a83a39693d3044646fa4eb36
Shriniwas Kulkarni <shriniwaskulkarni@Shriniwass-MacBook-Air.local>
2024-11-27 12:03:48 -0800

    TODOS

--- src/main.py
+++ src/main.py
@@ -2,3 +2,15 @@
 # alpha, beta, rho, Q, E, tasks, servers, phermones
 # E, phermones are matrices (len(tasks) * len(servers)) or it's transpose
 # tasks and servers are instances of the task and server in models
+
+# TODO : Shriniwas - instantiate all of the above constants and decide their values (which we will tweak)
+# Shriniwas - write thr ACO scheduler
+
+# TODO : Rahul - the main method where you will call the ACO scheduler and the random scheduler, call the plotting methods
+# Rahul - write the random scheduler.
+
+# TODO : Shinjini - write plotting methods in utils.py. The x coordinate will be num_epochs and y will be the cost components. Assume you will
+# get the x and y from Rahul's code in the main method
+
+# TODO : Anahita - Write the classes and constructors for models referring to the proposal. Also write a method that randomly initializes n
+# models (tasks + servers) with their attributes given n. Rahul will use this method when he tests for different n. Set seed for reproducibility.
