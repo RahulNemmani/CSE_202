b97a1cd860fb5b70c820dd42fdcdde2c9bb90567
Shriniwas Kulkarni <shriniwaskulkarni@Shriniwass-MacBook-Air.local>
2024-11-26 23:51:03 -0800

    Initial changes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,4 @@
-# main method
+# CONSTANTS
+# alpha, beta, rho, Q, E, tasks, servers, phermones
+# E, phermones are matrices (len(tasks) * len(servers)) or it's transpose
+# tasks and servers are instances of the task and server in models
--- src/task_scheduler.py
+++ src/task_scheduler.py
@@ -1,1 +1,24 @@
-#implement Task Scheduler using ACO here
+def makespan():
+    return
+
+def load_imbalance():
+    return
+
+def energy_usage():
+    return
+
+def cost():
+    makespan() + load_imbalance() + energy_usage() # weighted sum
+    return
+
+def heuristic(): # will be used in calculating server probability
+    return
+
+def server_probability(): # use the above heuristic and phermone matrix
+    return
+
+def ACO_Scheduler(alpha, beta, rho, Q, E, tasks, servers, phermones):
+    return
+
+def Random_Scheduler(tasks, servers): # random task allottment for baseline comparsion
+    return
