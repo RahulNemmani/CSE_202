126f5a587c46002b4f5ae3aab595214cc9c3c994
anasada <122490591+anasada@users.noreply.github.com>
2024-11-28 09:55:41 -0800

    Update server.py

--- src/models/server.py
+++ src/models/server.py
@@ -1,35 +1,35 @@
 import random
-random.seed(42) #set seed for reproducibility
+random.seed(42)
 
 class Server:
-    """
-    Each server has a resource capacity tuple (c_cpu,c_memory,c_storage).
-    """
+    """Each server has cpu, memory, and storage resource capacities."""
     
-    def __init__(self, c_cpu, c_memory, c_storage):
-        """Initialize the values of the resource capacity tuple.
-           The random values are 3-4x of the tasks."""
-        self.capacities = (c_cpu, c_memory, c_storage)
+    def __init__(self, cpu, memory, storage):
+        """Initialize the values of the resource capacities per server."""
+        self.cpu = cpu
+        self.memory = memory
+        self.storage = storage
         
-    
-    # Randomly initialize n servers with their attributes given n.
     def initServers(num_models):
         """
         Input:  number of servers to initialize
-        Output: set of servers S with capacity attributes
-        
-        to access the attributes (for x in S: x.capacities)
+        Output: list of servers S with capacity attributes each
         """
-        S = set() #servers
+        S = []
         
         for i in range(num_models):
             
-            # randomize attributes: need to be 3-4x of the tasks
+            # randomize attributes: 3-4 times greater than the tasks
             c_cpu = random.randint(300, 400)
             c_memory = random.uniform(400, 512)  
             c_storage = random.uniform(24000, 32000) 
             
-             # server_i initialized and added to S set
-            S.add(Server(c_cpu, c_memory, c_storage))
+             # server initialized and added to list of Servers
+            S.append(Server(c_cpu, c_memory, c_storage))
             
         return S
+    
+    def __getitem__(self, index):
+        """Access attributes with an index"""
+        resCap = [self.cpu, self.memory, self.storage]
+        return resCap[index]
