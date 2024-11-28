60c346a5248d8edfdeb85176575c59d324c6d24b
anasada <122490591+anasada@users.noreply.github.com>
2024-11-28 09:56:12 -0800

    Update task.py

--- src/models/task.py
+++ src/models/task.py
@@ -1,26 +1,22 @@
 import random
-random.seed(42) #set seed for reproducibility
+random.seed(42) 
 
 class Task:
-    """
-    Each task has a resource capacity tuple (c_cpu,c_memory,c_storage) and duration.
-    """
+    """Each task has cpu, memory, storage resource capacities AND duration."""
     
-    def __init__(self, c_cpu, c_memory, c_storage, duration):
-        """Initialize the values of the resource capacity tuple and duration."""
-        self.capacities = (c_cpu, c_memory, c_storage)
+    def __init__(self, cpu, memory, storage, duration):
+        """Initialize the values of the resource capacities and duration."""
+        self.cpu = cpu
+        self.memory = memory
+        self.storage = storage
         self.duration = duration
         
-        
-    # Randomly initialize n tasks with their attributes given n.
     def initTasks(num_models):
         """
         Input:  number of tasks to initialize
-        Output: set of tasks T with capacity attributes
-        
-        to access the attributes (for x in T: x.capacities)
+        Output: list of tasks T with capacity and duration attributes each
         """
-        T = set() #tasks
+        T = []
         
         for i in range(num_models):
             
@@ -28,9 +24,14 @@ class Task:
             c_cpu = random.randint(1, 100)
             c_memory = random.uniform(8, 128) 
             c_storage = random.uniform(5, 8000) 
-            duration = random.uniform(1, 2000) #not sure about these ranges
+            duration = random.uniform(1, 2000) 
             
-             # task_i initialized and added to T set
-            T.add(Task(c_cpu, c_memory, c_storage, duration))
+             # task initialized and added to list of Tasks
+            T.append(Task(c_cpu, c_memory, c_storage, duration))
             
         return T
+    
+    def __getitem__(self, index):
+        """Access attributes with an index"""
+        resCap = [self.cpu, self.memory, self.storage, self.duration]
+        return resCap[index]
